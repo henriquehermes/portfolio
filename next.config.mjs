e836e40b83b05f120c06ab3da79dbdba0a29693d
Henrique Hermes <henriqueh.97@hotmail.com>
2024-03-17 15:07:28 +0800

    up

--- next.config.mjs
+++ next.config.mjs
@@ -1,8 +1,8 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-	output: "export", // <=== enables static exports
+	// output: "export", // <=== enables static exports
 	reactStrictMode: true,
-	basePath: "/portfolio",
+	// basePath: "/portfolio",
 	images: {
 		unoptimized: true,
 	},
