0075fa5f9134a4b9c1f9413fb5d8d9455a393e1f
Henrique Hermes <henriqueh.97@hotmail.com>
2024-03-17 14:32:43 +0800

    fix some responsibility

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -1,7 +1,21 @@
-import { Box, Button, Flex, Text } from "@chakra-ui/react"
+import {
+	Box,
+	Button,
+	Flex,
+	IconButton,
+	Menu,
+	MenuButton,
+	MenuItem,
+	MenuList,
+	Text,
+	useMediaQuery,
+} from "@chakra-ui/react"
 import Container from "../Layout/Container"
+import { PiHamburger } from "react-icons/pi"
 
 const Header = () => {
+	const [isLargerThan900] = useMediaQuery("(min-width: 950px)")
+
 	return (
 		<Box bgColor="#1b1f24" w={"full"} color={"#fff"}>
 			<Container>
@@ -19,64 +33,108 @@ const Header = () => {
 						>{`Henrique Hermes`}</Text>
 					</Flex>
 
-					<Flex display={{ base: "none", lg: "flex" }} gap={10}>
-						<Button
-							variant="unstyled"
-							onClick={() => {
-								window.open(
-									"https://drive.google.com/file/d/1G5uPxvGYrfrClmnWqMQ-e4m5_zi0Xkbq/view"
-								)
-							}}
-						>
-							<Text fontSize={"20px"} color="#f0f2f4">
-								Resume
-							</Text>
-						</Button>
+					{!isLargerThan900 ? (
+						<Menu>
+							<MenuButton
+								as={IconButton}
+								aria-label="Options"
+								icon={<PiHamburger />}
+								variant="outline"
+								color="#fff"
+								fontSize={"25px"}
+								_active={{
+									background: "white",
+									color: "black",
+								}}
+							/>
+							<MenuList fontSize={"20px"} color="black">
+								<MenuItem
+									onClick={() => {
+										window.open(
+											"https://drive.google.com/file/d/1G5uPxvGYrfrClmnWqMQ-e4m5_zi0Xkbq/view"
+										)
+									}}
+								>
+									Resume
+								</MenuItem>
 
-						<Button
-							display="flex"
-							as={"a"}
-							href={"#about"}
-							variant="unstyled"
-						>
-							<Text fontSize={"20px"} color="#f0f2f4">
-								About
-							</Text>
-						</Button>
+								<MenuItem as="a" href="#about">
+									About
+								</MenuItem>
 
-						<Button
-							display="flex"
-							as={"a"}
-							href={"#skills"}
-							variant="unstyled"
-						>
-							<Text fontSize={"20px"} color="#f0f2f4">
-								My Skills
-							</Text>
-						</Button>
+								<MenuItem as="a" href="#skills">
+									My Skills
+								</MenuItem>
 
-						<Button
-							display="flex"
-							as={"a"}
-							href={"#portfolio"}
-							variant="unstyled"
-						>
-							<Text fontSize={"20px"} color="#f0f2f4">
-								My Portfolio
-							</Text>
-						</Button>
+								<MenuItem as="a" href="#portfolio">
+									My Portfolio
+								</MenuItem>
 
-						<Button
-							display="flex"
-							as={"a"}
-							href={"#contact"}
-							variant="unstyled"
-						>
-							<Text fontSize={"20px"} color="#f0f2f4">
-								Contact
-							</Text>
-						</Button>
-					</Flex>
+								<MenuItem as="a" href="#contact">
+									Contact
+								</MenuItem>
+							</MenuList>
+						</Menu>
+					) : (
+						<Flex gap={10}>
+							<Button
+								variant="unstyled"
+								onClick={() => {
+									window.open(
+										"https://drive.google.com/file/d/1G5uPxvGYrfrClmnWqMQ-e4m5_zi0Xkbq/view"
+									)
+								}}
+							>
+								<Text fontSize={"20px"} color="#f0f2f4">
+									Resume
+								</Text>
+							</Button>
+
+							<Button
+								display="flex"
+								as={"a"}
+								href={"#about"}
+								variant="unstyled"
+							>
+								<Text fontSize={"20px"} color="#f0f2f4">
+									About
+								</Text>
+							</Button>
+
+							<Button
+								display="flex"
+								as={"a"}
+								href={"#skills"}
+								variant="unstyled"
+							>
+								<Text fontSize={"20px"} color="#f0f2f4">
+									My Skills
+								</Text>
+							</Button>
+
+							<Button
+								display="flex"
+								as={"a"}
+								href={"#portfolio"}
+								variant="unstyled"
+							>
+								<Text fontSize={"20px"} color="#f0f2f4">
+									My Portfolio
+								</Text>
+							</Button>
+
+							<Button
+								display="flex"
+								as={"a"}
+								href={"#contact"}
+								variant="unstyled"
+							>
+								<Text fontSize={"20px"} color="#f0f2f4">
+									Contact
+								</Text>
+							</Button>
+						</Flex>
+					)}
 				</Flex>
 			</Container>
 		</Box>
